03abd17d5714a22b7a3fb9b1dddbe40b5c3dbf4c
onimeno62 <66439782+onimeno62@users.noreply.github.com>
2026-09-18 02:52:42 +0330

    fix(widgets): hydrate newly placed widget instances

--- app/src/main/java/com/example/ui/widgets/glance/CompactPlayerGlanceWidget.kt
+++ app/src/main/java/com/example/ui/widgets/glance/CompactPlayerGlanceWidget.kt
@@ -39,4 +39,13 @@ class CompactPlayerGlanceWidget : GlanceAppWidget() {
 
 class CompactPlayerWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = CompactPlayerGlanceWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: android.appwidget.AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        com.example.ui.widgets.updater.WidgetUpdateManager.requestWithFollowUp(context)
+    }
 }
