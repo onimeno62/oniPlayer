47c183d1b4e48455eb4bc4600da06264f3b6432c
onimeno62 <66439782+onimeno62@users.noreply.github.com>
2026-09-18 02:52:47 +0330

    fix(widgets): hydrate newly placed widget instances

--- app/src/main/java/com/example/ui/widgets/glance/LyricsGlanceWidget.kt
+++ app/src/main/java/com/example/ui/widgets/glance/LyricsGlanceWidget.kt
@@ -39,4 +39,13 @@ class LyricsGlanceWidget : GlanceAppWidget() {
 
 class LyricsWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = LyricsGlanceWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: android.appwidget.AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        com.example.ui.widgets.updater.WidgetUpdateManager.requestWithFollowUp(context)
+    }
 }
